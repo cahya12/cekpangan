3ee45da55f678fbc14a293cacce555b463ee567f
cahya12 <dwi.cahya12@gmail.com>
2019-10-02 06:01:44 +0700

    initial

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+// define("BASE_URL", "http://convictcloud.com/test/grubr/grubrApi/");
+define("NEWS_API_KEY", "15e25456c72544a6b8ce6acc515cb045");
+define("NEWS_API_URL", "https://newsapi.org/v2/");
+?>
